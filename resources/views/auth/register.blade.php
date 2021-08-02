c89b8e3ad3605e93ba30c9d22024ef7c1778719d
shohokimoto <s_kimoto@ifreajapan.co.jp>
2021-08-02 12:56:20 +0900

    2021.8.2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,12 +26,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="belongs" class="col-md-4 col-form-label text-md-right">所属</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="berongs" type="text" class="form-control" name="belongs" required autocomplete="organization" value="{{ old('belongs') }}">
 
-                                @error('email')
+                                @error('belongs')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
